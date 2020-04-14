21803f627670a0a305e9741a59fa1f9ac5b68139
hubishen <charles.chen@iress.com>
2020-04-14 16:37:13 +1000

    initial commit

--- components/message.js
+++ components/message.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         //alignItems: 'center',
-        //justifyContent: 'flex-start',
+        justifyContent: 'flex-start',
         backgroundColor: '#d3d3d3'
 
     },
